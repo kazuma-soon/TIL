00ec34357e588c1411fa832340214ffb7165ad1a
kazuma-soon <podradio2020@gmail.com>
2022-05-13 11:52:01 +0900

    Add: typescriptでのサーバー作成

--- typescript/sampleapp2/src/backend/index.ts
+++ typescript/sampleapp2/src/backend/index.ts
@@ -1,8 +1,18 @@
 import express from 'express'
+import messageController from './controllers/message'
+
 const app: express.Express = express()
-app.get("/", (req:express.Request, res:express.Response)=>{
-    res.send("こんにちは");
-})
-app.listen(3000,()=>{
-    console.log('ポート3000番で起動しました。')
+
+// 追加 サーバの強化追加
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// 追加終了
+
+// 追加 処理の外だし
+app.use("/messages", messageController)
+// 追加終了
+
+// 3000番ポートでAPIサーバ起動
+app.listen(3000, () => {
+    console.log('ポート3000番で起動しましたよ。')
 })
