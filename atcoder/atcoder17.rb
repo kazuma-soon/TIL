04a22b2cffc94db9225c889a30c319aad6f95b08
kazuma-soon <podradio2020@gmail.com>
2022-05-15 20:59:59 +0900

    Add: atcoder23

--- atcoder/atcoder17.rb
+++ atcoder/atcoder17.rb
@@ -1,11 +1,3 @@
-puts gets.split.map(&:to_i).sort == [5, 5, 7] ? 'YES' : 'NO'
-
-group = gets.split.map(&:to_i).group_by(&:itself)
-puts group[5]&.length == 2 && group[7]&.length == 1 ? 'YES' : 'NO'
-
-, l = gets.split.map(&:to_i)
-uts n.times.map {gets.chomp}.sort.join
-
 n, k = gets.split.map(&:to_i)
 d = gets.chomp.split
  
